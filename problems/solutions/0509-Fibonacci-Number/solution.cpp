43ba75a942bb8483cd747e0f3d2307287a9bcfd5
LeonardoSantosBR <leosantosmelo07@gmail.com>
2025-05-22 17:24:17 -0300

    fix: ajuste solution.cpp 0509

--- problems/solutions/0509-Fibonacci-Number/solution.cpp
+++ problems/solutions/0509-Fibonacci-Number/solution.cpp
@@ -13,6 +13,11 @@ public:
     };
     int calc(int n)
     {
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return 1;
+
         int a = n - 1;
         int b = n - 2;
         return frml(a) + frml(b);
