fb485d2fb939bcc7b294c96d8d9f578fcb583225
LeonardoSantosBR <leosantosmelo07@gmail.com>
2025-05-22 09:44:01 -0300

    feat: create cpp solution of 0509 problem

---
+++ problems/solutions/0509-Fibonacci-Number/solution.cpp
@@ -0,0 +1,25 @@
+
+class Solution
+{
+public:
+    int frml(int n)
+    {
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return 1;
+
+        return frml(n - 1) + frml(n - 2);
+    };
+    int calc(int n)
+    {
+        int a = n - 1;
+        int b = n - 2;
+        return frml(a) + frml(b);
+    };
+    int fib(int n)
+    {
+        int r = calc(n);
+        return r;
+    }
+};
