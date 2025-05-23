48d58094baa7aacdc5ef3750944a460d36cc5cc7
LeonardoSantosBR <leosantosmelo07@gmail.com>
2025-05-23 13:19:01 -0300

    fix: refatoracao solution 0509

--- problems/solutions/0509-Fibonacci-Number/solution.cpp
+++ problems/solutions/0509-Fibonacci-Number/solution.cpp
@@ -1,30 +1,19 @@
 
-class Solution
-{
+
+class Solution {
 public:
-    int frml(int n)
-    {
+    int fib(int n) {
         if (n == 0)
             return 0;
         if (n == 1)
             return 1;
 
-        return frml(n - 1) + frml(n - 2);
-    };
-    int calc(int n)
-    {
-        if (n == 0)
-            return 0;
-        if (n == 1)
-            return 1;
-
-        int a = n - 1;
-        int b = n - 2;
-        return frml(a) + frml(b);
-    };
-    int fib(int n)
-    {
-        int r = calc(n);
-        return r;
+        int a = 0, b = 1, c;
+        for (int i = 2; i <= n; i++) {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        return b;
     }
 };
